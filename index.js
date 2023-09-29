653bd4f08012c2e79690610aa36942051ed23dad
Izzah-Salam <izzahsalam00@gmail.com>
2023-09-29 03:44:00 -0700

    simple tod list app

--- index.js
+++ index.js
@@ -1,28 +1,78 @@
+// const addBtn = document.querySelector(".addbtn");
+// let listItem = [];
+//     function getInput() {
+//       const inputEl = document.getElementById("input");
+//       listItem.push(inputEl.value);
+//       addData(listItem);
+//       inputEl.value = ""
+//     }
+
+//     function addData(listItem) {
+//       let listItemsHTML = ""; // Initialize an empty string to accumulate list items
+//       listItem.forEach(list => {
+//         listItemsHTML += `
+//           <div class="listitem">
+//             <div class="listvalue">${list}</div>
+//             <button class="crosBtn">X</button>
+//           </div>`;
+//       });
+//       showData(listItemsHTML);
+//     }
+
+//     function showData(listItemsHTML) {
+//       const addListItem = document.querySelector(".list");
+//       addListItem.innerHTML = listItemsHTML;
+//     }
+
+
+//     addBtn.addEventListener("click", getInput);
+
+
 const addBtn = document.querySelector(".addbtn");
-let listItem = [];
+    let listItem = [];
+
+    // Load stored data from local storage
+    if (localStorage.getItem("todoList")) {
+      listItem = JSON.parse(localStorage.getItem("todoList"));
+      showData(generateHTML());
+    }
+
     function getInput() {
       const inputEl = document.getElementById("input");
       listItem.push(inputEl.value);
       addData(listItem);
-      inputEl.value = ""
+      inputEl.value = "";
+
+      // Save the updated data to local storage
+      localStorage.setItem("todoList", JSON.stringify(listItem));
     }
 
     function addData(listItem) {
+      showData(generateHTML());
+    }
+
+    function generateHTML() {
       let listItemsHTML = ""; // Initialize an empty string to accumulate list items
-      listItem.forEach(list => {
+      listItem.forEach((list, index) => {
         listItemsHTML += `
           <div class="listitem">
             <div class="listvalue">${list}</div>
-            <button class="crosBtn">X</button>
+            <button class="crosBtn" onclick="removeItem(${index})">X</button>
           </div>`;
       });
-      showData(listItemsHTML);
+      return listItemsHTML;
     }
 
     function showData(listItemsHTML) {
       const addListItem = document.querySelector(".list");
       addListItem.innerHTML = listItemsHTML;
     }
-    
+
+    // Remove item from the list and update local storage
+    function removeItem(index) {
+      listItem.splice(index, 1);
+      showData(generateHTML());
+      localStorage.setItem("todoList", JSON.stringify(listItem));
+    }
 
     addBtn.addEventListener("click", getInput);
